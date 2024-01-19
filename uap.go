96e2a48c7674cf411956e07a123e89a5fa915ae5
Caleb Rowland <cjrowland4@gmail.com>
2024-01-19 08:41:09 -0600

    Commit stage 5 of 7

--- uap.go
+++ uap.go
@@ -8,83 +8,134 @@ import (
 )
 
 type Applicant struct {
-	firstName   string
-	lastName    string
-	gpa         float64
-	choice1     string
-	choice2     string
-	choice3     string
-	depAccepted string
+	firstName      string
+	lastName       string
+	pref1          string
+	pref2          string
+	pref3          string
+	depAccepted    string
+	physicsScore   float64
+	chemistryScore float64
+	mathScore      float64
+	csScore        float64
 }
 
 func main() {
-	applicants := getApplicants()
 	departmentCapacity := readInt()
+	applicants := getApplicants()
 
-	admittedApplicants := executeAdmissionsProcess(&applicants, departmentCapacity)
+	admittedApplicants := executeAdmissionsProcess(applicants, departmentCapacity)
 	printAdmitted(admittedApplicants)
 	return
 }
 
-func executeAdmissionsProcess(applicants *[]Applicant, departmentCapacity int) (admittedApplicants []Applicant) {
+func executeAdmissionsProcess(apps []Applicant, departmentCapacity int) (admittedApplicants []Applicant) {
 	departments := map[string]int{
-		"Physics":     0,
-		"Mathematics": 0,
-		"Engineering": 0,
-		"Chemistry":   0,
 		"Biotech":     0,
+		"Chemistry":   0,
+		"Engineering": 0,
+		"Mathematics": 0,
+		"Physics":     0,
 	}
 
 	// Round 1
-	for i, applicant := range *applicants {
-		if (departments[applicant.choice1] < departmentCapacity) && ((*applicants)[i].depAccepted == "") {
-			(*applicants)[i].depAccepted = applicant.choice1
-			departments[applicant.choice1]++
-			admittedApplicants = append(admittedApplicants, (*applicants)[i])
+	sort.Slice(apps, func(i, j int) bool {
+		if examScore(apps[i], apps[i].pref1) != examScore(apps[j], apps[j].pref1) {
+			return examScore(apps[i], apps[i].pref1) > examScore(apps[j], apps[j].pref1)
+		}
+		if apps[i].firstName != apps[j].firstName {
+			return apps[i].firstName < apps[j].firstName
+		}
+
+		return apps[i].lastName < apps[j].lastName
+	})
+	for i, applicant := range apps {
+		if (departments[applicant.pref1] < departmentCapacity) && (apps[i].depAccepted == "") {
+			apps[i].depAccepted = applicant.pref1
+			departments[applicant.pref1]++
+			admittedApplicants = append(admittedApplicants, apps[i])
 		}
 	}
 
 	// Round 2
-	for i, applicant := range *applicants {
-		if (departments[applicant.choice2] < departmentCapacity) && ((*applicants)[i].depAccepted == "") {
-			(*applicants)[i].depAccepted = applicant.choice2
-			departments[applicant.choice2]++
-			admittedApplicants = append(admittedApplicants, (*applicants)[i])
+	sort.Slice(apps, func(i, j int) bool {
+		if examScore(apps[i], apps[i].pref2) != examScore(apps[j], apps[j].pref2) {
+			return examScore(apps[i], apps[i].pref2) > examScore(apps[j], apps[j].pref2)
+		}
+		if apps[i].firstName != apps[j].firstName {
+			return apps[i].firstName < apps[j].firstName
+		}
+
+		return apps[i].lastName < apps[j].lastName
+	})
+	for i, applicant := range apps {
+		if (departments[applicant.pref2] < departmentCapacity) && (apps[i].depAccepted == "") {
+			apps[i].depAccepted = applicant.pref2
+			departments[applicant.pref2]++
+			admittedApplicants = append(admittedApplicants, apps[i])
 		}
 	}
 
 	// Round 3
-	for i, applicant := range *applicants {
-		if (departments[applicant.choice3] < departmentCapacity) && ((*applicants)[i].depAccepted == "") {
-			(*applicants)[i].depAccepted = applicant.choice3
-			departments[applicant.choice3]++
-			admittedApplicants = append(admittedApplicants, (*applicants)[i])
+	sort.Slice(apps, func(i, j int) bool {
+		if examScore(apps[i], apps[i].pref3) != examScore(apps[j], apps[j].pref3) {
+			return examScore(apps[i], apps[i].pref3) > examScore(apps[j], apps[j].pref3)
+		}
+		if apps[i].firstName != apps[j].firstName {
+			return apps[i].firstName < apps[j].firstName
+		}
+
+		return apps[i].lastName < apps[j].lastName
+	})
+	for i, applicant := range apps {
+		if (departments[applicant.pref3] < departmentCapacity) && (apps[i].depAccepted == "") {
+			apps[i].depAccepted = applicant.pref3
+			departments[applicant.pref3]++
+			admittedApplicants = append(admittedApplicants, apps[i])
 		}
 	}
 
 	return admittedApplicants
 }
 
-func printAdmitted(applicants []Applicant) {
-	sort.Slice(applicants, func(i, j int) bool {
-		if applicants[i].depAccepted != applicants[j].depAccepted {
-			return applicants[i].depAccepted < applicants[j].depAccepted
+func examScore(applicant Applicant, department string) (score float64) {
+	switch department {
+	case "Physics":
+		return applicant.physicsScore
+	case "Biotech", "Chemistry":
+		return applicant.chemistryScore
+	case "Mathematics":
+		return applicant.mathScore
+	case "Engineering":
+		return applicant.csScore
+	default:
+		return 0.0
+	}
+}
+
+func printAdmitted(apps []Applicant) {
+	sort.Slice(apps, func(i, j int) bool {
+		if apps[i].depAccepted != apps[j].depAccepted {
+			return apps[i].depAccepted < apps[j].depAccepted
 		}
-		if applicants[i].gpa != applicants[j].gpa {
-			return applicants[i].gpa > applicants[j].gpa
+		if examScore(apps[i], apps[i].depAccepted) != examScore(apps[j], apps[j].depAccepted) {
+			return examScore(apps[i], apps[i].depAccepted) > examScore(apps[j], apps[j].depAccepted)
 		}
-		if applicants[i].firstName != applicants[j].firstName {
-			return applicants[i].firstName < applicants[j].firstName
+		if apps[i].firstName != apps[j].firstName {
+			return apps[i].firstName < apps[j].firstName
 		}
 
-		return applicants[i].lastName < applicants[j].lastName
+		return apps[i].lastName < apps[j].lastName
 	})
 
 	for _, dep := range [5]string{"Biotech", "Chemistry", "Engineering", "Mathematics", "Physics"} {
 		fmt.Println(dep)
-		for _, applicant := range applicants {
+		for _, applicant := range apps {
 			if applicant.depAccepted == dep {
-				fmt.Printf("%s %s %.2f\n", applicant.firstName, applicant.lastName, applicant.gpa)
+				fmt.Printf("%s %s %.1f\n",
+					applicant.firstName,
+					applicant.lastName,
+					examScore(applicant, applicant.depAccepted))
 			}
 		}
 		fmt.Println()
@@ -101,24 +152,20 @@ func getApplicants() (applicants []Applicant) {
 	for scanner.Scan() {
 		var newApplicant Applicant
 		line := scanner.Text()
-		fmt.Sscanf(line, "%s %s %f %s %s %s",
+		fmt.Sscanf(line, "%s %s %f %f %f %f %s %s %s",
 			&newApplicant.firstName,
 			&newApplicant.lastName,
-			&newApplicant.gpa,
-			&newApplicant.choice1,
-			&newApplicant.choice2,
-			&newApplicant.choice3)
+			&newApplicant.physicsScore,
+			&newApplicant.chemistryScore,
+			&newApplicant.mathScore,
+			&newApplicant.csScore,
+			&newApplicant.pref1,
+			&newApplicant.pref2,
+			&newApplicant.pref3,
+		)
 
 		applicants = append(applicants, newApplicant)
 	}
-
-	sort.Slice(applicants, func(i, j int) bool {
-		if applicants[i].gpa != applicants[j].gpa {
-			return applicants[i].gpa > applicants[j].gpa
-		}
-
-		return (applicants[i].firstName + applicants[i].lastName) < (applicants[j].firstName + applicants[j].lastName)
-	})
 
 	return applicants
 }
